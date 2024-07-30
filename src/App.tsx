4cd6940ee92f5d69af23ffe3717c11fdfb0e6eb0
Brenda Volkart <volkarttbrenda@gmail.com>
2024-07-30 18:58:21 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: black;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Testando</Text>
+      <TextNew>NOVO TESTE</TextNew>
     </SafeAreaView>
   );
 };
