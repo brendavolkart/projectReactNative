728a03d4b0ce32b3972a79cde685f643a553338a
Brenda Volkart <volkarttbrenda@gmail.com>
2024-08-02 17:21:17 -0300

    feat: create input shared

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
 
+import Input from '../../../shared/components/input/input';
+import { ContainerLogin } from '../styles/login.style';
+
 const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <ContainerLogin>
+        <Text>Oi brenda</Text>
+        <Input />
+      </ContainerLogin>
     </View>
   );
 };
