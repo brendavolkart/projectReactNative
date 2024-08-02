af0fbd29caff732f57b971ceae9e6b7da71d723b
Brenda Volkart <volkarttbrenda@gmail.com>
2024-08-02 18:17:38 -0300

    feat: creat shared button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,19 @@
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('entrouuuuuuuuuuuuuuuu');
+  };
   return (
     <View>
       <ContainerLogin>
         <Text>Oi brenda</Text>
         <Input />
+        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
