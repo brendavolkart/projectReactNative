188352304bfbcf133936d374b61a7050e149854e
Brenda Volkart <volkarttbrenda@gmail.com>
2024-08-05 17:46:49 -0300

    feat: insert loading and disabled in button

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,38 +1,61 @@
-import { TouchableOpacityProps } from 'react-native';
+import { ActivityIndicator, TouchableOpacityProps } from 'react-native';
 
 import { theme } from '../../themes/theme';
 import Text from '../text/Text';
 import { textTypes } from '../text/textTypes';
-import { ButtonContainer, ButtonSecundary, GradientButton } from './button.style';
+import { ButtonContainer, ButtonDisabled, ButtonSecundary, GradientButton } from './button.style';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
   margin?: string;
   type: string;
+  disabled?: boolean;
+  loading?: boolean;
+  onPress?: () => void;
 }
 
-const Button = ({ title, type, margin, ...props }: ButtonProps) => {
+const Button = ({ title, type, disabled, loading, onPress, margin, ...props }: ButtonProps) => {
+  const handleOnPress = () => {
+    if (!loading && !disabled && onPress) {
+      onPress();
+    }
+  };
+  const renderText = (color: string) => (
+    <>
+      {loading ? (
+        <ActivityIndicator color={theme.colors.neutralTheme.white} />
+      ) : (
+        <Text type={textTypes.BUTTON_BOLD} color={color}>
+          {title}
+        </Text>
+      )}
+    </>
+  );
+
+  if (disabled) {
+    return (
+      <ButtonDisabled {...props} margin={margin}>
+        {renderText(theme.colors.neutralTheme.white)}
+      </ButtonDisabled>
+    );
+  }
   switch (type) {
     case theme.buttons.buttonsTheme.secundary:
       return (
-        <ButtonSecundary margin={margin} {...props}>
-          <Text type={textTypes.BUTTON_BOLD} color={theme.colors.mainTheme.primary}>
-            {title}
-          </Text>
+        <ButtonSecundary {...props} margin={margin} onPress={handleOnPress}>
+          {renderText(theme.colors.mainTheme.primary)}
         </ButtonSecundary>
       );
     case theme.buttons.buttonsTheme.primary:
     default:
       return (
-        <ButtonContainer margin={margin} {...props}>
+        <ButtonContainer {...props} margin={margin} onPress={handleOnPress}>
           <GradientButton
             start={{ x: 0.0, y: 0.0 }}
             end={{ x: 1.0, y: 1.0 }}
             colors={[theme.colors.purpleTheme.purple80, theme.colors.pinkTheme.pink80]}
           >
-            <Text type={textTypes.BUTTON_BOLD} color={theme.colors.neutralTheme.white}>
-              {title}
-            </Text>
+            {renderText(theme.colors.neutralTheme.white)}
           </GradientButton>
         </ButtonContainer>
       );
