5fc9414012a092a33457ac64e091fc2b7b766e84
Brenda Volkart <volkarttbrenda@gmail.com>
2024-08-19 22:22:08 -0300

    feat: validate email and password in login screen

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,30 +3,42 @@ import { View } from 'react-native';
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
 import { theme } from '../../../shared/themes/theme';
+import { useLogin } from '../hooks/useLogin';
 import { ContainerLogin, ImageLogo } from '../styles/login.style';
 
 const Login = () => {
-  const handleOnPress = async () => {
-    console.log('entrouuuuuuuuuuuuuuuu');
-  };
+  const {
+    email,
+    password,
+    loading,
+    errorMessage,
+    handleOnPress,
+    handleOnChangeEmail,
+    handleOnChangePassword,
+  } = useLogin();
   return (
     <View>
       <ContainerLogin>
         <ImageLogo resizeMode="cover" source={require('../../../assets/images/logo2.png')} />
         <Input
-          //errorMessage="Usuário ou senha inválidos"
+          value={email}
+          errorMessage={errorMessage}
           margin="0px 0px 8px 0px"
           placeholder="Digite seu email"
           title="Email:"
+          onChange={handleOnChangeEmail}
         />
         <Input
-          //errorMessage="Usuário ou senha inválidos"
+          value={password}
+          errorMessage={errorMessage}
           secureTextEntry
           placeholder="Digite sua senha"
           title="Senha:"
+          onChange={handleOnChangePassword}
         />
         <Button
           type={theme.buttons.buttonsTheme.primary}
+          loading={loading}
           margin="16px"
           title="ENTRAR"
           onPress={handleOnPress}
