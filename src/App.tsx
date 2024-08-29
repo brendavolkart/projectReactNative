9642194f7d97313e21afb5765484c098a9de3f40
Brenda Volkart <volkarttbrenda@gmail.com>
2024-08-29 12:04:33 -0300

    feat: insert navigation object

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
 import { Provider } from 'react-redux';
 
-import Home from './modules/home';
-import Login from './modules/login';
+import Navigation from './Navigation';
 import GlobalModal from './shared/components/modal/globalModal/globalModal';
 import store from './store';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
       <GlobalModal />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation />
     </Provider>
-    //provider armazena todos os dados
   );
 };
 
